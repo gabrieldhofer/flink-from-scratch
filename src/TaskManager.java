8414bce9e1a95e09d65e349c15b660455a538694
gabrieldhofer <leirbagabriel468@gmail.com>
2025-04-12 14:28:29 -0500

    gg

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,9 +1,29 @@
 
 class TaskManager.java {
-	/**
-	 * Author: Gabriel Hofer
-	 * Date: 2025-04-12
+	/** 
+	 *	Author: Gabriel Hofer
+	 *	Date: 2025-04-12
 	 */
+
+	MemoryAndIOManager memoryManager;
+	NetworkManager networkManager;
+	ActorSystem actorSystem;
+
+	class TaskSlot {
+		/** Author: Gabriel Hofer, Date: 2025-04-12 */
+	}
+
+	class MemoryAndIOManager {
+		/** Author: Gabriel Hofer, Date: 2025-04-12 */
+	}
+
+	class NetworkManager {
+		/** Author: Gabriel Hofer, Date: 2025-04-12 */
+	}
+
+	class ActorSystem {
+		/** Author: Gabriel Hofer, Date: 2025-04-12 */
+	}
 
 }
 
